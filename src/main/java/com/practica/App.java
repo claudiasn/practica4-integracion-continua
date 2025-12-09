71a2839f60701770bc5fd61e13b51b4eac40c197
claudiasn <123484438+claudiasn@users.noreply.github.com>
2025-12-09 11:03:00 +0100

    Update App.java

--- src/main/java/com/practica/App.java
+++ src/main/java/com/practica/App.java
@@ -1,8 +1,15 @@
 package com.practica;
 
-
-public class App {
+public class App 
+{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");  // código duplicado
+    }
+
+    // Método duplicado para asegurar fallo en SonarCloud
+    public void metodoDuplicado() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");  // código duplicado
     }
 }
