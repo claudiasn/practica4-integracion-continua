babbac6c32b2b1a266d59f17ea67d9479d891008
claudiasn <123484438+claudiasn@users.noreply.github.com>
2025-12-10 17:28:34 +0100

    Update AppTest.java

--- src/test/java/com/practica/AppTest.java
+++ src/test/java/com/practica/AppTest.java
@@ -3,12 +3,10 @@ package com.practica;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-public class AppTest 
-{
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue(true); // TEST CORRECTO PARA QUE PASE
+    public void shouldAnswerWithTrue() {
+        assertTrue(true); // TEST QUE PASA
     }
 }
-
