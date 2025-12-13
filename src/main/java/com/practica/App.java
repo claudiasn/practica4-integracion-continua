f20b64fafb31d5c2fd090c90f177dad1a4d65ef8
joselopggg <josealejanlg11@gmail.com>
2025-12-14 00:38:09 +0100

    Update App.java

--- src/main/java/com/practica/App.java
+++ src/main/java/com/practica/App.java
@@ -7,10 +7,14 @@ public class App {
         System.out.println(app.calcular());
     }
 
+    /**
+     * Método de ejemplo que realiza una operación simple.
+     * @return resultado de la operación
+     */
     public int calcular() {
         int a = 5;
         int b = 1;
 
-        return a / b; // operación segura, sin warnings
+        return a / b;
     }
 }
