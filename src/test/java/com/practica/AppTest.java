34b8983b184e6bde8cb66f6a9f7786988a86acbd
inesCarrascoL <icarrasco1@alu.ucam.edu>
2025-12-16 15:50:23 +0100

    Update test to expect 9 instead of 8

--- src/test/java/com/practica/AppTest.java
+++ src/test/java/com/practica/AppTest.java
@@ -7,6 +7,6 @@ public class AppTest {
 
     @Test
     public void shouldAnswerWithTrue() {
-        assertEquals(8, new App().calcular());
+        assertEquals(9, new App().calcular());
     }
 }
